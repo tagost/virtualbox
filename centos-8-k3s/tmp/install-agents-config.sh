ee4dcfeb3744fc0b40a9314fa1fda8d79ec8d426
Guillermo Sosa <guillesos@hotmail.es>
2022-03-22 10:42:49 -0300

    update

--- centos-8-k3s/tmp/install-agents-config.sh
+++ centos-8-k3s/tmp/install-agents-config.sh
@@ -1,4 +1,4 @@
 export INSTALL_K3S_VERSION=v1.20.14-rc1+k3s2
 export K3S_URL=https://192.168.0.5:6443
 export INSTALL_K3S_EXEC="--node-ip 192.168.0.6 --node-external-ip 192.168.0.6"
-export K3S_TOKEN=K108d533af328af05facde939d13501cbd2e782bcb5255c69c8bc0c5c3308dc1904::server:417ab98ed0459cd5639e050c72a116f8
+export K3S_TOKEN=K10e1eb19249534c6222784133a365f35fdbedd7affa114e645bac58e10ca5d6f53::server:a10ef960011eed431e9b98dae6bf9640
